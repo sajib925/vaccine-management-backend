eefa6b255b55ea771ed0001bd5d971e9076a5040
sajib925 <sajibahmed1004@gmail.com>
2024-08-13 21:31:42 +0600

    add postgressql and change requirenment

--- vaccine_management/settings.py
+++ vaccine_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,12 +106,20 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://vaccine_management_db_f3zz_user:pfKPG6WQrY93KaZvusxC155WAMUcNhjq@dpg-cqtnmp2j1k6c738q3dp0-a.oregon-postgres.render.com/vaccine_management_db_f3zz',
+    )
 }
+
 
 
 # Password validation
