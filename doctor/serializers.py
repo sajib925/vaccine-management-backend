a40e8afd3b53e58cfcc1e30113c99f591eeb11b6
sajib925 <sajibahmed1004@gmail.com>
2024-08-13 22:53:32 +0600

    doctor serializers and views change

--- doctor/serializers.py
+++ doctor/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from . import models
+from .models import Doctor
 
 class DoctorSerializer(serializers.ModelSerializer):
-    # user = serializers.StringRelatedField(many=False)
-    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    # The user is automatically set in the view, so exclude it from the input fields
     class Meta:
-        model = models.Doctor
+        model = Doctor
         fields = '__all__'
+        read_only_fields = ['user']
 
+    def create(self, validated_data):
+        user = self.context['request'].user
+        doctor = Doctor.objects.create(user=user, **validated_data)
+        return doctor
--- doctor/views.py
+++ doctor/views.py
@@ -2,8 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from . import models
-from . import serializers
+from . import models, serializers
 
 class DoctorListCreateApiView(APIView):
     def get(self, request):
@@ -11,31 +10,30 @@ class DoctorListCreateApiView(APIView):
         serializer = serializers.DoctorSerializer(doctors, many=True)
         return Response(serializer.data)
 
-
     def post(self, request):
-        serializer = serializers.DoctorSerializer(data=request.data)
+        serializer = serializers.DoctorSerializer(data=request.data, context={'request': request})
 
         if serializer.is_valid():
-            # Save the doctor with the logged-in user
-            serializer.save(user=request.user)
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class DoctorDetailApiView(APIView):
     def get(self, request, pk):
         doctor = get_object_or_404(models.Doctor, pk=pk)
         serializer = serializers.DoctorSerializer(doctor)
         return Response(serializer.data)
-    
+
     def put(self, request, pk):
         doctor = get_object_or_404(models.Doctor, pk=pk)
-        serializer = serializers.DoctorSerializer(doctor, data=request.data)
+        serializer = serializers.DoctorSerializer(doctor, data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
     def delete(self, request, pk):
         doctor = get_object_or_404(models.Doctor, pk=pk)
         doctor.delete()
